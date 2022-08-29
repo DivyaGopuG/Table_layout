ab073171ae9ebb315b215ed49e19e138ea19cec8
DivyaGopuG <54533758+DivyaGopuG@users.noreply.github.com>
2022-08-29 21:37:37 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/table_layout/MainActivity.java
+++ app/src/main/java/com/example/table_layout/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // to enable forgot password link in the xml file...
         TextView linkTextView = findViewById(R.id.activity_main_link);
         linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
-    }
 
+        EditText eu = findViewById(R.id.eUsername);
+        EditText ep = findViewById(R.id.ePassword);
+        Button b = findViewById(R.id.b1);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (eu.getText().toString().equals("admin") && ep.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Valid User", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Invalid User", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
 }
